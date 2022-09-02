4699ff980a466c885f12b0d9bd3c258d9fd3d06b
siam50 <siamurrahman50@gmail.com>
2022-09-03 00:33:01 +0600

    show category added

--- apps.js
+++ apps.js
@@ -12,7 +12,7 @@ const displayCategory = (categories) => {
     categories.forEach(category => {
         const li = document.createElement('li');
         li.innerHTML = `
-        <a class="nav-link active" aria-current="page" href="#">${category.category_name}   </a>
+        <a onclick="loadCategoryId(${category.category_id})" class="nav-link active" aria-current="page" href="#">${category.category_name}   </a>
         `
         ul.appendChild(li);
         // console.log(category)
@@ -21,3 +21,52 @@ const displayCategory = (categories) => {
 }
 
 loadCategory();
+
+const loadCategoryId = (data) => {
+    startSpinner(true);
+    const url = `https://openapi.programming-hero.com/api/news/category/${0}${data}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayCategoryId(data.data))
+
+}
+
+const displayCategoryId = (datas) => {
+    const cardContainer = document.getElementById('card-container');
+    cardContainer.innerHTML = ``;
+    datas.forEach(perId => {
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="card">
+                        <img src="${perId.thumbnail_url}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">${perId.title}</h5>
+                            <p class="card-text">${perId.details.slice(0, 100)} .....</p>
+                            <div><img src="${perId.author.img}" class="card-img-top w-25 h-25 rounded-circle me-3" alt="...">
+                            <span class="text-primary me-5"> ${perId.author.name ? perId.author.name : 'No Author'} </span>
+                            <span> Views: ${perId.total_view ? perId.total_view : 'No Views'} </span>
+                            </div>
+                            <button type="button" class="btn btn-primary mt-3" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                Show Details
+                            </button>
+                        </div>
+                    </div>
+        `
+        cardContainer.appendChild(div);
+        console.log(perId)
+        startSpinner(false);
+    });
+
+}
+
+// Spinner Part<<<<<<<<<<<<<<<
+const startSpinner = (isLoading) => {
+    const showSpinner = document.getElementById('show-spinner');
+    if (isLoading) {
+        showSpinner.classList.remove('d-none');
+    }
+    else {
+        showSpinner.classList.add('d-none');
+    }
+}
