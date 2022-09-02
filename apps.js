40d7248caffa936b38537968fa8a155459556bf3
siam50 <siamurrahman50@gmail.com>
2022-09-02 22:28:20 +0600

    category added

--- apps.js
+++ apps.js
@@ -1,1 +1,23 @@
-console.log('hello')
+const loadCategory = () => {
+    const url = `https://openapi.programming-hero.com/api/news/categories`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayCategory(data.data.news_category))
+    // .catch(error=> console.log(error));
+}
+
+const displayCategory = (categories) => {
+    const ul = document.getElementById('ul');
+
+    categories.forEach(category => {
+        const li = document.createElement('li');
+        li.innerHTML = `
+        <a class="nav-link active" aria-current="page" href="#">${category.category_name}   </a>
+        `
+        ul.appendChild(li);
+        // console.log(category)
+    });
+
+}
+
+loadCategory();
